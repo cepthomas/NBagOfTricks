90ca715e17cccbfdf2884053f3294a4b9a5e3408
cepthomas <cepthomas@gmail.com>
2021-07-20 12:09:44 -0500

    Improved mmtimer.

--- Core/MmTimerEx.cs
+++ Core/MmTimerEx.cs
@@ -15,24 +15,30 @@ namespace NBagOfTricks
     /// </summary>
     public class MmTimerEx : IDisposable
     {
-        class TimerInstance
-        {
-            /// <summary>The time between events in msec.</summary>
-            public int period = 10;
+        //class TimerInstance
+        //{
+        //    /// <summary>The requested time between events in msec.</summary>
+        //    public int period = 10;
 
-            /// <summary>Actual accumulated msec.</summary>
-            public double current = 0.0;
-        }
+        //    /// <summary>Actual accumulated msec.</summary> 
+        //    public double current = 0.0;
+        //}
 
         #region Fields
         /// <summary>All the timer instances. Key is id.</summary>
-        readonly Dictionary<string, TimerInstance> _timers = new Dictionary<string, TimerInstance>();
+        //readonly Dictionary<string, TimerInstance> _timers = new Dictionary<string, TimerInstance>();
 
         /// <summary>Used for more accurate timing.</summary>
         readonly Stopwatch _sw = new Stopwatch();
 
+        /// <summary>Multimedia timer identifier. -1 is not inited, 0 is fail to init, other is valid id.</summary>
+        int _timerID = -1;
+
         /// <summary>Indicates whether or not the timer is running.</summary>
         bool _running = false;
+
+        /// <summary>Stopwatch support.</summary>
+        long _startTicks = -1;
 
         /// <summary>Stopwatch support.</summary>
         long _lastTicks = -1;
@@ -42,68 +48,101 @@ namespace NBagOfTricks
 
         /// <summary>Msec for mm timer tick.</summary>
         const int MMTIMER_PERIOD = 1;
+
+        /// <summary>Called by Windows when a mm timer event occurs.</summary>
+        readonly NativeMethods.TimeProc _timeProc;
         #endregion
+
 
         #region Events
-        /// <summary>Occurs when the time period has elapsed.</summary>
-        public event EventHandler<TimerEventArgs> TimerElapsedEvent;
+        ///// <summary>Occurs when the time period has elapsed.</summary>
+        //public event EventHandler<TimerEventArgs> TimerElapsedEvent;
 
-        /// <summary>FastTimer event args.</summary>
-        public class TimerEventArgs : EventArgs
+        ///// <summary>Timer event args.</summary>
+        //public class TimerEventArgs : EventArgs
+        //{
+        //    /// <summary>Elapsed timers.</summary>
+        //    public List<string> ElapsedTimers { get; set; } = new List<string>();
+        //}
+        #endregion
+
+
+
+
+        //public event EventHandler<TimerEventArgs> TimerElapsedEventX;
+        public delegate void TimeProcX(double totalElapsed, double periodElapsed);
+
+        class TimerInstanceX
         {
-            /// <summary>Elapsed timers.</summary>
-            public List<string> ElapsedTimers { get; set; } = new List<string>();
+            /// <summary>The requested time between events in msec.</summary>
+            public int period = 10;
+
+            /// <summary>User handler.</summary>
+            public TimeProcX handler = null;
+
+            /// <summary>Actual accumulated msec since last event.</summary> 
+            public double elapsed = 0.0;
         }
-        #endregion
 
-        #region Internal support for multimedia timer
-        /// <summary>Multimedia timer identifier. -1 is not inited, 0 is fail to init, other is valid id.</summary>
-        int _timerID = -1;
+        List<TimerInstanceX> _timersX = new List<TimerInstanceX>();
 
-        /// <summary>
-        /// Timer resolution in milliseconds. The resolution increases with smaller values - a resolution of 0
-        /// indicates periodic events should occur with the greatest possible accuracy. To reduce system 
-        /// overhead, however, you should use the maximum value appropriate for your application.
-        /// </summary>
-        readonly int _resolution = 1;
+        public void SetTimer(int period, TimeProcX handler)
+        {
+            _timersX.Add(new TimerInstanceX
+            {
+                period = period,
+                handler = handler
+            });
+        }
 
-        /// <summary>
-        /// Called by Windows when a mm timer event occurs.
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="msg"></param>
-        /// <param name="user"></param>
-        /// <param name="param1"></param>
-        /// <param name="param2"></param>
-        delegate void TimeProc(int id, int msg, int user, int param1, int param2);
 
-        /// <summary>
-        /// Called by Windows when a mm timer event occurs.
-        /// </summary>
-        readonly TimeProc _timeProc;
-        #endregion
 
-        #region Win32 Multimedia Timer Functions
-        #pragma warning disable IDE1006 // Naming Styles
 
-        [DllImport("winmm.dll")]
-        private static extern int timeGetDevCaps(ref TimerCaps caps, int sizeOfTimerCaps);
+        #region Native Methods
+        /// <summary>Win32 Multimedia Timer Functions.</summary>
+        internal class NativeMethods
+        {
+            #pragma warning disable IDE1006 // Naming Styles
 
-        /// <summary>Start her up.</summary>
-        /// <param name="delay">Event delay, in milliseconds.If this value is not in the range of the minimum and maximum event delays supported by the timer, the function returns an error.</param>
-        /// <param name="resolution">Resolution of the timer event, in milliseconds. The resolution increases with smaller values; a resolution of 0 indicates periodic events should occur with the greatest possible accuracy. To reduce system overhead, however, you should use the maximum value appropriate for your application.</param>
-        /// <param name="proc">Pointer to a callback function that is called once upon expiration of a single event or periodically upon expiration of periodic events.</param>
-        /// <param name="user">User-supplied callback data.</param>
-        /// <param name="mode">Timer event type.</param>
-        /// <returns></returns>
-        [DllImport("winmm.dll")]
-        private static extern int timeSetEvent(int delay, int resolution, TimeProc proc, IntPtr user, int mode);
+            /// <summary></summary>
+            /// <param name="caps"></param>
+            /// <param name="sizeOfTimerCaps"></param>
+            /// <returns></returns>
+            [DllImport("winmm.dll")]
+            internal static extern int timeGetDevCaps(ref TimerCaps caps, int sizeOfTimerCaps);
 
-        [DllImport("winmm.dll")]
-        private static extern int timeKillEvent(int id);
+            /// <summary>Start her up.</summary>
+            /// <param name="delay">Event delay, in milliseconds.If this value is not in the range of the minimum and maximum event delays supported by the timer, the function returns an error.</param>
+            /// <param name="resolution">Resolution of the timer event, in milliseconds. The resolution increases with smaller values; a resolution of 0 indicates periodic events should occur with the greatest possible accuracy. To reduce system overhead, however, you should use the maximum value appropriate for your application.</param>
+            /// <param name="proc">Pointer to a callback function that is called once upon expiration of a single event or periodically upon expiration of periodic events.</param>
+            /// <param name="user">User-supplied callback data.</param>
+            /// <param name="mode">Timer event type.</param>
+            /// <returns></returns>
+            [DllImport("winmm.dll")]
+            internal static extern int timeSetEvent(int delay, int resolution, TimeProc proc, IntPtr user, int mode);
 
-        private const int TIMERR_NOERROR = 0;
-        #pragma warning restore IDE1006 // Naming Styles
+            /// <summary></summary>
+            /// <param name="id"></param>
+            /// <returns></returns>
+            [DllImport("winmm.dll")]
+            internal static extern int timeKillEvent(int id);
+
+            /// <summary>Called by Windows when a mm timer event occurs.</summary>
+            internal delegate void TimeProc(int id, int msg, int user, int param1, int param2);
+
+            /// <summary>Represents information about the multimedia timer capabilities.</summary>
+            [StructLayout(LayoutKind.Sequential)]
+            internal struct TimerCaps
+            {
+                /// <summary>Minimum supported period in milliseconds.</summary>
+                public int periodMin;
+
+                /// <summary>Maximum supported period in milliseconds.</summary>
+                public int periodMax;
+            }
+
+            #pragma warning restore IDE1006 // Naming Styles
+        }
         #endregion
 
         #region Lifecycle
@@ -118,7 +157,7 @@ namespace NBagOfTricks
             }
 
             // Initialize timer with default values.
-            _timeProc = new TimeProc(MmTimerCallback);
+            _timeProc = new NativeMethods.TimeProc(MmTimerCallback);
         }
 
         /// <summary>
@@ -155,35 +194,35 @@ namespace NBagOfTricks
         #endregion
 
         #region Public functions
-        /// <summary>
-        /// Add a new timer instance.
-        /// </summary>
-        /// <param name="id">Arbitrary id as string</param>
-        /// <param name="period">Period in msec </param>
-        public void SetTimer(string id, int period)
-        {
-            _timers[id] = new TimerInstance
-            {
-                period = period,
-                current = 0
-            };
-        }
+        ///// <summary>
+        ///// Add a new timer instance.
+        ///// </summary>
+        ///// <param name="id">Arbitrary id as string</param>
+        ///// <param name="period">Period in msec </param>
+        //public void SetTimer(string id, int period)
+        //{
+        //    _timers[id] = new TimerInstance
+        //    {
+        //        period = period,
+        //        current = 0
+        //    };
+        //}
 
         /// <summary>
         /// Starts the periodic timer.
         /// </summary>
         public void Start()
         {
-            foreach (TimerInstance t in _timers.Values)
+            foreach (TimerInstanceX t in _timersX)
             {
-                t.current = 0;
+                t.elapsed = 0;
             }
 
             // Clean up first.
             Stop();
 
-            // Create and start periodic timer.
-            _timerID = timeSetEvent(MMTIMER_PERIOD, _resolution, _timeProc, IntPtr.Zero, 1); // 1=TIME_PERIODIC
+            // Create and start periodic timer. resolution is 0 or 1, mode is 1=TIME_PERIODIC
+            _timerID = NativeMethods.timeSetEvent(MMTIMER_PERIOD, 0, _timeProc, IntPtr.Zero, 1);
 
             // If the timer was created successfully.
             if (_timerID > 0)
@@ -207,8 +246,8 @@ namespace NBagOfTricks
             // Stop and destroy timer.
             if(_timerID > 0)
             {
-                int result = timeKillEvent(_timerID);
-                // result != TIMERR_NOERROR
+                int result = NativeMethods.timeKillEvent(_timerID);
+                // result != TIMERR_NOERROR <> 0
                 _timerID = -1;
             }
             _running = false;
@@ -218,7 +257,7 @@ namespace NBagOfTricks
 
         #region Private functions
         /// <summary>
-        /// Multimedia timer callback. Don't trust the accuracy of the mm timer so measure actual using a stopwatch.
+        /// System multimedia timer callback. Don't trust the accuracy of the mm timer so measure actual using a stopwatch.
         /// </summary>
         /// <param name="id">The identifier of the timer. The identifier is returned by the timeSetEvent function.</param>
         /// <param name="msg">Reserved.</param>
@@ -233,54 +272,50 @@ namespace NBagOfTricks
                 {
                     // When are we?
                     long t = _sw.ElapsedTicks; // snap
-                    double msec = (t - _lastTicks) * 1000D / Stopwatch.Frequency;
+
+                    double totalMsec = (t - _startTicks) * 1000D / Stopwatch.Frequency;
+                    double periodMsec = (t - _lastTicks) * 1000D / Stopwatch.Frequency;
                     _lastTicks = t;
 
-                    // Check for expirations.
-                    List<string> elapsed = new List<string>();
-                    // Allow for a bit of jitter around 0.
+                    // Check for expirations. Allow for a bit of jitter around 0.
                     double allowance = 0.5; // msec
 
-                    foreach(string tid in _timers.Keys)
+
+                    foreach (var timer in _timersX)
                     {
-                        TimerInstance timer = _timers[tid];
-                        timer.current += msec;
-                        if ((timer.period - timer.current) < allowance)
+                        timer.elapsed += periodMsec;
+                        if ((timer.period - timer.elapsed) < allowance)
                         {
-                            elapsed.Add(tid);
-                            timer.current = 0.0;
+                            // Notify.
+                            timer.handler(totalMsec, timer.elapsed);
+                            timer.elapsed = 0.0;
                         }
                     }
 
-                    if (elapsed.Count > 0)
-                    {
-                        TimerElapsedEvent?.Invoke(this, new TimerEventArgs() { ElapsedTimers = elapsed });
-                    }
+                    //foreach(string tid in _timers.Keys)
+                    //{
+                    //    TimerInstance timer = _timers[tid];
+                    //    timer.current += msec;
+                    //    if ((timer.period - timer.current) < allowance)
+                    //    {
+                    //        elapsed.Add(tid);
+                    //        timer.current = 0.0;
+                    //    }
+                    //}
+
+                    //if (elapsed.Count > 0)
+                    //{
+                    //    TimerElapsedEvent?.Invoke(this, new TimerEventArgs() { ElapsedTimers = elapsed });
+                    //}
                 }
                 else
                 {
                     // Starting.
-                    _lastTicks = _sw.ElapsedTicks;
+                    _startTicks = _sw.ElapsedTicks;
+                    _lastTicks = _startTicks;
                 }
             }
         }
         #endregion
-
-        /// <summary>
-        /// Represents information about the multimedia timer capabilities.
-        /// </summary>
-        [StructLayout(LayoutKind.Sequential)]
-        struct TimerCaps
-        {
-            /// <summary>
-            /// Minimum supported period in milliseconds.
-            /// </summary>
-            public int periodMin;
-
-            /// <summary>
-            /// Maximum supported period in milliseconds.
-            /// </summary>
-            public int periodMax;
-        }
     }
 }
