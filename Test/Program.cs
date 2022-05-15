c45e036f6a35bd003f11c3d31f768f751124f81e
cepthomas <cepthomas@gmail.com>
2022-05-15 13:31:39 -0500

    Reorganized.

--- Test/Program.cs
+++ Test/Program.cs
@@ -13,8 +13,8 @@ namespace NBagOfTricks.Test
         {
             // Use pnut for automated lib tests.
             TestRunner runner = new TestRunner(OutputFormat.Readable);
-            var cases = new[] { "UTILS_MISC" };
-            //var cases = new[] { "PNUT", "UTILS", "CMD", "MMTEX", "IPC", "TOOLS", "JSON" };
+            //var cases = new[] { "UTILS_MISC" };
+            var cases = new[] { "PNUT", "UTILS", "CMD", "MMTEX", "IPC", "TOOLS", "JSON" };
             runner.RunSuites(cases);
             File.WriteAllLines(@"test_out.txt", runner.Context.OutputLines);
         }
