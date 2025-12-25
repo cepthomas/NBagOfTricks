125ba6802077cb2c437f49eb5bedac5a874c32b8
cepthomas <cepthomas@gmail.com>
2025-12-25 17:54:52 -0600

    Color names.

--- Test/Test_JSON.cs
+++ Test/Test_JSON.cs
@@ -26,16 +26,16 @@ namespace Ephemera.NBagOfTricks.Test
             JsonSerializerOptions opts = new() { WriteIndented = true };
             string json = JsonSerializer.Serialize(ct1, opts);
 
-            UT_EQUAL(json, "{\r\n  \"ControlColor\": \"MediumOrchid\",\r\n  \"Position\": \"101.1,532.22\",\r\n  \"SomeRect\": \"34,55,890,75\",\r\n  \"PrettyFace\": \"Cooper Black,9\"\r\n}");
+            UT_EQUAL(json, "{\r\n  \"DrawColor\": \"MediumOrchid\",\r\n  \"Position\": \"101.1,532.22\",\r\n  \"SomeRect\": \"34,55,890,75\",\r\n  \"PrettyFace\": \"Cooper Black,9\"\r\n}");
 
-            var ser = "{\"ControlColor\": \"Pink\", \"Position\": \"45.88,502.01\", \"SomeRect\": \"505,80,75,66\", \"PrettyFace\": \"Arial, 14\"}";
+            var ser = "{\"DrawColor\": \"Pink\", \"Position\": \"45.88,502.01\", \"SomeRect\": \"505,80,75,66\", \"PrettyFace\": \"Arial, 14\"}";
 
             ConverterTarget? ct2 = JsonSerializer.Deserialize<ConverterTarget>(ser);
 
             UT_NOT_NULL(ct2);
             if(ct2 is not null)
             {
-                UT_EQUAL(ct2.ControlColor.Name, "Pink");
+                UT_EQUAL(ct2.DrawColor.Name, "Pink");
                 UT_CLOSE(ct2.Position.X, 45.88, 0.00001);
                 UT_EQUAL(ct2.SomeRect.Width, 75);
                 UT_EQUAL(ct2.PrettyFace.Size, 14);
@@ -47,7 +47,7 @@ namespace Ephemera.NBagOfTricks.Test
     public class ConverterTarget
     {
         [JsonConverter(typeof(JsonColorConverter))]
-        public Color ControlColor { get; set; } = Color.MediumOrchid;
+        public Color DrawColor { get; set; } = Color.MediumOrchid;
 
         [JsonConverter(typeof(JsonPointFConverter))]
         public PointF Position { get; set; } = new(101.1f, 532.22f);
