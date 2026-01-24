7fc97cb3dda0aba7b4b1aa0c8a1ad5038fcd63af
cepthomas <cepthomas@gmail.com>
2026-01-24 12:28:57 -0600

    Simplify console colors.

--- ColorUtils.cs
+++ ColorUtils.cs
@@ -8,30 +8,6 @@ using System.Linq;
 
 namespace Ephemera.NBagOfTricks
 {
-    #region Types
-    /// <summary>ConsoleColor variation with None added.</summary>
-    public enum ConsoleColorEx
-    {
-        None = -1,
-        Black = ConsoleColor.Black,
-        DarkBlue = ConsoleColor.DarkBlue,
-        DarkGreen = ConsoleColor.DarkGreen,
-        DarkCyan = ConsoleColor.DarkCyan,
-        DarkRed = ConsoleColor.DarkRed,
-        DarkMagenta = ConsoleColor.DarkMagenta,
-        DarkYellow = ConsoleColor.DarkYellow,
-        Gray = ConsoleColor.Gray,
-        DarkGray = ConsoleColor.DarkGray,
-        Blue = ConsoleColor.Blue,
-        Green = ConsoleColor.Green,
-        Cyan = ConsoleColor.Cyan,
-        Red = ConsoleColor.Red,
-        Magenta = ConsoleColor.Magenta,
-        Yellow = ConsoleColor.Yellow,
-        White = ConsoleColor.White
-    }
-    #endregion
-
     public static class ColorUtils
     {
         static Dictionary<(int, int, int), KnownColor>? _colorLUT = null;
